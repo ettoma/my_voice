cd68719d8d799558da58c5cc52c5f7f335a79774
EttoreToma <ettore.toma@gmail.com>
2021-08-12 21:29:17 +0200

    State management OK

--- lib/pages/audio_player.dart
+++ lib/pages/audio_player.dart
@@ -20,99 +20,11 @@ class _AudioPlayerState extends State<AudioPlayer> {
   final player = SoundPlayer();
 
   List<FileSystemEntity>? file;
-  List fileURLs = [];
-  List fileNames = [];
   Directory? directory;
-  // AudioFiles audioFileList = AudioFiles();
-  List<RecordedFile>? fileData;
 
   void getDirectory() async {
     directory = await getApplicationDocumentsDirectory();
     file = directory!.listSync(recursive: true);
-    // fileData = audioFileList.fileList;
-
-    setState(
-      () {
-        // for (var element in fileData!) {}
-        // for (var element in file!) {
-        //   if (element.path.contains('.Trash')) {
-        //   } else {
-        //     String fileName = element.path.split('/').last.split('.aac').first;
-        //     fileURLs.add(fileName);
-        //     DateTime parsedDate =
-        //         DateTime.fromMillisecondsSinceEpoch(int.parse(fileName));
-        //     final format = DateFormat('dd.MM.yyyy HH:mm');
-        //     final formatDate = format.format(parsedDate);
-        //     fileNames.add(formatDate);
-        //   }
-        // }
-        // fileData!.map((e) => fileNames.add(e.fileName));
-      },
-    );
-    // fileData!.map(
-    //   (e) {
-    //     DateTime parsedDate = DateTime.fromMillisecondsSinceEpoch(1);
-    //     final format = DateFormat('dd.MM.yyyy HH:mm');
-    //     final formatDate = format.format(parsedDate);
-    //     fileNames.add(formatDate);
-    //   },
-    // );
-    // for (var element in file!) {
-    //   if (element.path.contains('.Trash')) {
-    //   } else {
-    //     String fileName = element.path.split('/').last.split('.aac').first;
-    //     fileURLs.add(fileName);
-    //   }
-    // }
-
-    // getListFiles();
-  }
-
-  ListView getListFiles() {
-    return ListView.builder(
-      itemCount: fileNames.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                fileNames[index],
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              IconButton(
-                onPressed: () async {
-                  await player.togglePlaying(
-                      whenFinished: () => setState(() {}),
-                      fileName: directory!.uri.toFilePath(windows: false) +
-                          fileURLs[index] +
-                          '.aac');
-                  setState(() {});
-                },
-                icon: const FaIcon(
-                  FontAwesomeIcons.play,
-                  size: 18,
-                  color: Colors.black38,
-                ),
-              ),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      final targetFile =
-                          File("${directory!.path}/${fileURLs[index]}.aac");
-                      targetFile.deleteSync(recursive: true);
-                      fileURLs.remove(fileURLs[index]);
-                      fileNames.remove(fileNames[index]);
-                    });
-                  },
-                  icon: const FaIcon(FontAwesomeIcons.trashAlt))
-            ],
-          ),
-        );
-      },
-    );
   }
 
   @override
@@ -132,20 +44,51 @@ class _AudioPlayerState extends State<AudioPlayer> {
   Widget build(BuildContext context) {
     final provider = Provider.of<AudioFileModel>(context);
     final files = provider.fileList;
+    final format = DateFormat('dd.MM.yyyy HH:mm');
+
     return Scaffold(
       appBar: appBar(),
-      // body: Column(
-      //   children: [
-      //     Expanded(
-      //         child: fileNames.isEmpty
-      //             ? const Text('Go record something')
-      //             : getListFiles()),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Text(files[index].fileName);
-          },
-          separatorBuilder: (context, index) => Container(height: 8),
-          itemCount: files.length),
+      body: files.isEmpty
+          ? const Center(
+              child: Text('No audio files to play'),
+            )
+          : ListView.separated(
+              itemBuilder: (context, index) {
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(format
+                        .format(DateTime.fromMillisecondsSinceEpoch(
+                            int.parse(files[index].fileName)))
+                        .toString()),
+                    IconButton(
+                      icon: const FaIcon(FontAwesomeIcons.play),
+                      onPressed: () async {
+                        await player.togglePlaying(
+                            whenFinished: () => setState(() {}),
+                            fileName:
+                                directory!.uri.toFilePath(windows: false) +
+                                    files[index].fileName +
+                                    '.aac');
+                        setState(() {});
+                      },
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            final targetFile = File(
+                                "${directory!.path}/${files[index].fileName}.aac");
+                            targetFile.deleteSync(recursive: true);
+                            files.remove(files[index]);
+                          });
+                        },
+                        icon: const FaIcon(FontAwesomeIcons.trashAlt))
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) => Container(height: 8),
+              itemCount: files.length),
     );
   }
 }
