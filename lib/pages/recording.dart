85432dd69cef3df6063f3da0b8822b77bd004769
EttoreToma <ettore.toma@gmail.com>
2021-08-29 22:37:02 +0200

    new animation started

--- lib/pages/recording.dart
+++ lib/pages/recording.dart
@@ -17,6 +17,9 @@ class Recording extends StatefulWidget {
 
 class _RecordingState extends State<Recording> {
   final recorder = SoundRecorder();
+  bool _isButtonDisabled = false;
+  int number = 5;
+  Timer? animationTimer;
 
   @override
   void initState() {
@@ -51,76 +54,101 @@ class _RecordingState extends State<Recording> {
                     fontSize: AppColours().titleSize,
                     fontWeight: FontWeight.w500),
               ),
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.amberAccent,
+                child: Center(child: Text(number.toString())),
+              ),
               TextButton(
-                onPressed: () {
-                  recorder.record();
-                  Timer(
-                    Duration(seconds: 5),
-                    () async {
-                      recorder.stop();
-                      recorder.stop();
-                      await showDialog(
-                        barrierDismissible: false,
-                        context: context,
-                        builder: (context) => Dialog(
-                          child: Container(
-                            padding: EdgeInsets.all(12),
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            width: MediaQuery.of(context).size.width * 0.85,
-                            child: Column(
-                              children: [
-                                TextField(
-                                  controller: _moodTextController,
-                                  onChanged: (e) =>
-                                      mood = _moodTextController.text,
-                                  decoration:
-                                      InputDecoration(labelText: 'Mood'),
+                onPressed: _isButtonDisabled == true
+                    ? null
+                    : () {
+                        _isButtonDisabled = true;
+                        setState(() {});
+                        recorder.record();
+                        animationTimer =
+                            Timer.periodic(Duration(seconds: 1), (timer) {
+                          setState(() {
+                            number = number - 1;
+                          });
+                          if (number == 0) {
+                            animationTimer!.cancel();
+                          }
+                        });
+                        Timer(
+                          Duration(seconds: 5),
+                          () async {
+                            recorder.stop();
+                            await showDialog(
+                              barrierDismissible: false,
+                              context: context,
+                              builder: (context) => Dialog(
+                                child: Container(
+                                  padding: EdgeInsets.all(12),
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.4,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.85,
+                                  child: Column(
+                                    children: [
+                                      TextField(
+                                        controller: _moodTextController,
+                                        onChanged: (e) =>
+                                            mood = _moodTextController.text,
+                                        decoration:
+                                            InputDecoration(labelText: 'Mood'),
+                                      ),
+                                      TextField(
+                                        controller: _tagTextController,
+                                        onChanged: (e) =>
+                                            tag = _tagTextController.text,
+                                        decoration:
+                                            InputDecoration(labelText: 'Tag'),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        icon: FaIcon(FontAwesomeIcons.check),
+                                      )
+                                    ],
+                                  ),
                                 ),
-                                TextField(
-                                  controller: _tagTextController,
-                                  onChanged: (e) =>
-                                      tag = _tagTextController.text,
-                                  decoration: InputDecoration(labelText: 'Tag'),
-                                ),
-                                IconButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  icon: FaIcon(FontAwesomeIcons.check),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                      recorder.updateDB(mood.isNotEmpty ? mood : '',
-                          tag.isNotEmpty ? tag : '');
+                              ),
+                            );
+                            recorder.updateDB(mood.isNotEmpty ? mood : '',
+                                tag.isNotEmpty ? tag : '');
+                            _isButtonDisabled = false;
+                            setState(() {});
 
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return AudioPlayer();
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return AudioPlayer();
+                                },
+                              ),
+                            );
                           },
-                        ),
-                      );
-                    },
-                  );
-                },
+                        );
+                      },
                 child: Text('start recording'),
               ),
               TextButton(
                 child: Text('Player'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return AudioPlayer();
+                onPressed: _isButtonDisabled == true
+                    ? null
+                    : () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return AudioPlayer();
+                            },
+                          ),
+                        );
                       },
-                    ),
-                  );
-                },
               )
             ],
           ),
